cafa487581d067777af80d779e94519777e08c28
Jake Hracho <jhracho@nd.edu>
2022-03-22 11:35:19 -0400

    added basic sqlalchemy calls for reference

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from .endpoints import endpoint
 from .auth import auth
+
+'''
+engine = create_engine()
+Session = sessionmaker(bind=engine)
+'''
 
 def create_app():
     cors = CORS()
--- backend/auth.py
+++ backend/auth.py
@@ -1,20 +1,47 @@
 from flask import Blueprint, request
 
+from . import Session, engine
+from .models import User
+from sqlalchemy import insert
+from werkzeug.security import generate_password_hash, check_password_hash
+
 auth = Blueprint('auth', __name__)
 
 # Log in a user based on supplied credentials
-@auth.route('/authenticate-login', methods=['POST'])
+@auth.route('/login', methods=['POST'])
 def login():
     # Get request data  
-    user = request.json.get('username')
+    username = request.json.get('username')
     pswd = request.json.get('password')
+
+    if username is None or pswd is None:
+        return {'auth':False, 'msg':'One or more fields is left empty...'}
 
     # Check with db
     #TODO: sqlalchemy call
+    '''
+    session = Session()
+    user = session.query(User).filter(username=user).first()
+    session.close()
+    if user:
+        if check_password_hash(pswd, user.password):
+            return {'auth':True}
+    return {'auth':False, 'msg':'Incorrect username and/or password...'}
+    '''
     dU = 'jhracho'
     dP = 'test'
 
-    if (dU == user and dP == pswd):
+    if (dU == username and dP == pswd):
         return {'auth':True}  
     else:
-         return {'auth':False}
+         return {'auth':False, 'msg':'Incorrect username and/or password...'}
+
+@auth.route('/signup', methods=['POST'])
+def signup():
+    '''
+    session = Session()
+    newUser = User(put in column data here)
+    session.add(newUser)
+    session.close()
+    '''
+    return 
