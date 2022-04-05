981c58897f13a7c6d5884f9fee2499cd49a1f07a
Jake Hracho <jhracho@nd.edu>
2022-04-05 13:26:41 -0400

    fixed games page for now

--- frontend/src/Components/Listings/GameListing.js
+++ frontend/src/Components/Listings/GameListing.js
@@ -7,19 +7,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFootball, faFutbol, faBasketball, faGolfBallTee, faPerson, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 const GameListing = (props) =>{
-    const sport = props.game['sport'];
-
+    const game = props.game;
+    const sport = game['sport'];
+    let icon;
+    switch(sport){
+        case 'Football':
+            icon = <FontAwesomeIcon icon={faFootball} />;
+            break;
+        case 'Basketball':
+            icon = <FontAwesomeIcon icon={faBasketball} />;
+            break;
+        case 'Soccer':
+            icon = <FontAwesomeIcon icon={faFutbol} />;
+            break;
+        case 'Golf':
+            icon = <FontAwesomeIcon icon={faGolfBallTee} />;
+            break;
+        default:
+            icon = <FontAwesomeIcon icon={faGolfBallTee} />;
+            break;
+    }
     return(    
         <Card className='game-card'>
-            <Card.Header as='h5'><FontAwesomeIcon icon={faFootball} /> Football - <cite>Fun 3v3 Game</cite></Card.Header>
+            <Card.Header as='h5'>{icon} {game['sport']} - <cite>{game['name']}</cite></Card.Header>
             <Card.Body>
                 <Card.Text>
-                Game Description
+                {game['location']}
                 </Card.Text>
                 <div className = 'info-field'>
-                <h5><FontAwesomeIcon icon={faPerson} /> : 99</h5> 
-                <h5><FontAwesomeIcon icon={faCalendar} /> : 12/12/2022 00:00</h5>
-                <Button variant="primary"><a href={'/game/' + props.game['id']}><h3>Game Link</h3></a></Button>
+                <h5><FontAwesomeIcon icon={faPerson} /> : {game['needed']}</h5> 
+                <h5><FontAwesomeIcon icon={faCalendar} /> : {game['date']} 00:00</h5>
+                <Button variant="success" className = 'game-link-button'><a href={'/game/' + game['id']}><h3>Join Game</h3></a></Button>
+                <Button variant="primary" className = 'game-link-button'><a href={'/game/' + game['id']}><h3>More Info</h3></a></Button>
                 </div>
             </Card.Body>
         </Card>
