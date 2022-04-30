ae1d29dddf1eaf8e15a8ae5327c3c0f4e9779de2
Jake Hracho <jhracho@nd.edu>
2022-04-30 13:28:57 -0400

    added loading icon back to teams page

--- frontend/src/Components/Home/YourTeams.js
+++ frontend/src/Components/Home/YourTeams.js
@@ -3,14 +3,13 @@ import TeamCard from './TeamCard';
 
 const YourTeams = (props) =>{
     var teams = props.teams;
-    
     return(
         <div className='your-teams homeCol'>
             <h2>Your Teams</h2>
             {teams.length === 1 && teams[0].name === '' &&(
                 <img className='loading-icon' src={require("../../Assets/loading.gif")} alt='Loading'/>
             )}
-            {teams.length === 1 && teams[0].name !== '' &&(
+            {teams.length > 0 && teams[0].name !== '' &&(
                 <Fragment>
                     {teams.map((team) => (
                         <TeamCard key={team.id} team={team} />
