e078f8a4b70c3a9eba2fa6f6184ce804ca7eb5c8
Jake Hracho <jhracho@nd.edu>
2022-05-01 16:06:51 -0400

    Fixed merge

--- frontend/src/Components/Teams/Teams.js
+++ frontend/src/Components/Teams/Teams.js
@@ -7,18 +7,11 @@ const Teams = () =>{
     return(
         <div>
             <NavBar active='Teams' />
-<<<<<<< HEAD
-            <div className='teams-page-body'>
-                <h1 className='teams-header'>Teams</h1>
-                <TeamsCardDeck />
-            </div>
-=======
             <h1>Teams</h1>
             <br />
             <AddTeamModal />
             <br />
             <SearchTeams />
->>>>>>> 5531045bb12030da7d2a1134c6fbe5afc48afbff
         </div>
     );
 };
