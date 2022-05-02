a6204b9f44c1b2597470e4139921f044f8c5b6c4
Jake Hracho <jhracho@nd.edu>
2022-05-01 21:05:00 -0400

    what

--- frontend/src/Components/Listings/Header.js
+++ frontend/src/Components/Listings/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPlus, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
